964326487a09887b946cab2d9f9a887701a75a9e
Brittani Baker <brittanibaker07@gmail.com>
2020-10-22 22:05:21 +0000

    Automatically backed up by Learn

--- lib/your_solution.rb
+++ lib/your_solution.rb
@@ -1,10 +1,6 @@
-Puts "Hello World!"
-ruby lib/your_solution.rb
+
 puts "Hello World!"
 
- # ./lib/your_solution.rb
-puts "Hello World!"
 
-p
 # This file should output "Hello World!" using a puts statement
 ruby lib/your_solution.rb
